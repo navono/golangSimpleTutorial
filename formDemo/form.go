a6a6a2c8bb8fc2a6aff7c04d49620c76ccde38ea
navono <navono007@gmail.com>
2018-01-20 15:21:51 +0800

    增加upload示例代码

--- formDemo/form.go
+++ formDemo/form.go
@@ -1,11 +1,16 @@
 package formDemo
 
 import (
+	"crypto/md5"
 	"fmt"
 	"html/template"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 	"strings"
+	"time"
 )
 
 func sayHello(w http.ResponseWriter, r *http.Request) {
@@ -36,9 +41,39 @@ func login(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func upload(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("method:", r.Method) //获取请求的方法
+	if r.Method == "GET" {
+		crutime := time.Now().Unix()
+		h := md5.New()
+		io.WriteString(h, strconv.FormatInt(crutime, 10))
+		token := fmt.Sprintf("%x", h.Sum(nil))
+
+		t, _ := template.ParseFiles("./formDemo/upload.html")
+		t.Execute(w, token)
+	} else {
+		r.ParseMultipartForm(32 << 20)
+		file, handler, err := r.FormFile("uploadfile")
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer file.Close()
+		fmt.Fprintf(w, "%v", handler.Header)
+		f, err := os.OpenFile("./formDemo/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666) // 此处假设当前目录下已存在test目录
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer f.Close()
+		io.Copy(f, file)
+	}
+}
+
 func init() {
 	http.HandleFunc("/", sayHello)   //设置访问的路由
 	http.HandleFunc("/login", login) //设置访问的路由
+	http.HandleFunc("/upload", upload)
 
 	fmt.Println("Http server listening at http://localhost:9090")
 	err := http.ListenAndServe(":9090", nil) //设置监听的端口
