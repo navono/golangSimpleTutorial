b60753bf6789d1370060e8ed418e8945ba3231dc
navono <navono007@gmail.com>
2018-01-21 17:35:53 +0800

    add livelocks sample code

--- main.go
+++ main.go
@@ -2,19 +2,13 @@ package main
 
 import (
 	"fmt"
-
-	_ "golangSimpleTutorial/sqlite"
+	// _ "golangSimpleTutorial/sqlite"
 	// _ "golangSimpleTutorial/formDemo"
 	// _ "golangSimpleTutorial/httpServer"
-	// _ "golangSimpleTutorial/concurrencyDemo"
+	_ "golangSimpleTutorial/concurrencyDemo"
 	// _ "golangSimpleTutorial/interfaceDemo"
 	// _ "golangSimpleTutorial/methodDemo"
 )
-
-func add(a int) int {
-	a = a + 1
-	return a
-}
 
 func main() {
 	fmt.Println("================= main =================")
